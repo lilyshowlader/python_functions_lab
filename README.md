ad570c4df02bda0cce1c9113ea704a181fe9d7bb
lilyshowlader <lilyshowlader@gmail.com>
2022-11-01 16:36:02 -0400

    first problem complete

--- README.md
+++
@@ -1,1 +0,0 @@
-# python_functions_lab
---
+++ python_functions.py
@@ -0,0 +1,16 @@
+# Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+
+def sum_to(n):
+  #returns the sum of the integers from 1 to n.
+  print(n * (n + 1) // 2)
+
+sum_to(6)  # returns 21
+sum_to(10) # returns 55
+
+
+
+# Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+# Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
+
+# Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on args.
